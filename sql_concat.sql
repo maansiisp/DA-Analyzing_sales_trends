2e1a179a0d45ec25183fa1615e7697889212bdde
maansiisp <159099467+maansiisp@users.noreply.github.com>
2024-02-06 13:03:18 +0530

    Delete sql_concat.sql

--- sql_concat.sql
+++
@@ -1,6 +0,0 @@
- //create a running total of standard_amt_usd over order time w.r.t the year and partition by that same year-truncated first occurred_at. //
- 
- SELECT standard_amt_usd,
-       DATE_TRUNC('year', occurred_at) as year,
-       SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) AS running_total
-FROM orders
