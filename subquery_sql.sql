59feb69bb8ff4ef2fd0a8b3d7fe58fd0be3a87cc
maansiisp <159099467+maansiisp@users.noreply.github.com>
2024-02-06 12:55:26 +0530

    Add files via upload
    with SUB-QUERY :
    
    1. To Provide the name of the sales representative in each region with the largest amount of total amount used in sales.

---
+++ subquery_sql.sql
@@ -0,0 +1,26 @@
+//Provide the name of the sales_rep in each region with the largest amount of total amount spent in sales.//
+
+
+SELECT t3.rep_name, t3.region_name, t3.total_amt
+FROM(SELECT region_name, MAX(total_amt) total_amt
+        FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+                FROM sales_reps s
+                JOIN accounts a
+                ON a.sales_rep_id = s.id
+                JOIN orders o
+                ON o.account_id = a.id
+                JOIN region r
+                ON r.id = s.region_id
+                GROUP BY 1, 2) t1
+        GROUP BY 1) t2
+JOIN (SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+        FROM sales_reps s
+        JOIN accounts a
+        ON a.sales_rep_id = s.id
+        JOIN orders o
+        ON o.account_id = a.id
+        JOIN region r
+        ON r.id = s.region_id
+        GROUP BY 1,2
+        ORDER BY 3 DESC) t3
+ON t3.region_name = t2.region_name AND t3.total_amt = t2.total_amt;
